ea2538d8562b6741bc598c7e1a3a90a80f97f57d
Rondgrt <rondgrt@gmail.com>
2019-01-19 15:07:17 +0100

    adding test class with beforeEach annotation

---
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -0,0 +1,28 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GreetingTest {
+
+    private Greeting greeting;
+
+    @BeforeEach
+    void setUp() {
+        System.out.println("In before each....");
+
+        greeting = new Greeting();
+    }
+
+    @Test
+    void helloWorld() {
+        System.out.println(greeting.helloWorld());
+    }
+
+    @Test
+    void helloWorld1() {
+        System.out.println(greeting.helloWorld("Ronnie"));
+    }
+}
